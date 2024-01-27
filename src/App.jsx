52ae4e54179b632c7b4ce889d2a262aae7eb90c8
ramiro999 <ramiro2191920@correo.uis.edu.co>
2024-01-27 15:55:16 -0500

    changes style

--- src/App.jsx
+++ src/App.jsx
@@ -3,94 +3,110 @@ import MoonIcon from "./components/MoonIcon";
 import CrossIcon from "./components/icons/CrossIcon";
 
 function App() {
-  return (
-    <div
-      className="min-h-screen 
+    return (
+        <>
+            <div
+                className="min-h-screen 
     bg-gray-300 bg-[url('./assets/images/bg-mobile-light.jpg')] 
     bg-contain bg-no-repeat"
-    >
-      <header className="container mx-auto px-4 ">
-        <div className="flex justify-between">
-          <h1 className="text-3xl font-semibold uppercase tracking-[0.3rem] text-white">
-            Todo
-          </h1>
-          <button>
-            <MoonIcon />
-          </button>
-        </div>
+            >
+                {/*  Header */}
+                <header className="container mx-auto px-4 ">
+                    <div className="flex justify-between">
+                        <h1 className="text-3xl font-semibold uppercase tracking-[0.3rem] text-white">
+                            Todo
+                        </h1>
+                        <button>
+                            <MoonIcon />
+                        </button>
+                    </div>
+                </header>
 
-        <form
-          className=" mt-8 flex items-center gap-4 overflow-hidden
+                <main className="container mx-auto mt-8 px-4">
+                    {/*  TodoCreate*/}
+                    <form
+                        className="flex items-center gap-4 overflow-hidden
          rounded-md bg-white px-4 py-4"
-        >
-          <span className="inline-block h-5 w-5 rounded-full border-2"></span>
-          <input
-            type="text"
-            placeholder="Create a new todo..."
-            className="w-full text-gray-400 outline-none"
-          />
-        </form>
-      </header>
+                    >
+                        <span className="inline-block h-5 w-5 rounded-full border-2"></span>
+                        <input
+                            type="text"
+                            placeholder="Create a new todo..."
+                            className="w-full text-gray-400 outline-none"
+                        />
+                    </form>
 
-      <main className="container mx-auto mt-8 px-4">
-        <div className="rounded-md bg-white [&>article]:p-4 ">
-          <article className="flex gap-4 border-b border-b-gray-400 px-4 py-4">
-            <button
-              className="inline-block h-5 w-5 flex-none 
+                    {/* TodoList (TodoItem) */}
+                    <div className="mt-8 rounded-md bg-white [&>article]:p-4  ">
+                        <article className="flex gap-4 border-b border-b-gray-400 px-4 py-4">
+                            <button
+                                className="inline-block h-5 w-5 flex-none 
             rounded-full border-2"
-            ></button>
-            <p className="grow text-gray-600">
-              Complete online Javascript curse in bluuweb
-            </p>
-            <button>
-              <CrossIcon />
-            </button>
-          </article>
+                            ></button>
+                            <p className="grow text-gray-600">
+                                Complete online Javascript curse in bluuweb
+                            </p>
+                            <button>
+                                <CrossIcon />
+                            </button>
+                        </article>
 
-          <article className="flex gap-4 border-b border-b-gray-400 px-4 py-4">
-            <button
-              className="inline-block h-5 w-5 flex-none 
+                        <article className="flex gap-4 border-b border-b-gray-400 px-4 py-4">
+                            <button
+                                className="inline-block h-5 w-5 flex-none 
             rounded-full border-2"
-            ></button>
-            <p className="grow text-gray-600">
-              Complete online Javascript curse in bluuweb
-            </p>
-            <button>
-              <CrossIcon />
-            </button>
-          </article>
+                            ></button>
+                            <p className="grow text-gray-600">
+                                Complete online Javascript curse in bluuweb
+                            </p>
+                            <button>
+                                <CrossIcon />
+                            </button>
+                        </article>
 
-          <article className="flex gap-4 border-b border-b-gray-400 px-4 py-4">
-            <button
-              className="inline-block h-5 w-5 flex-none 
+                        <article className="flex gap-4 border-b border-b-gray-400 px-4 py-4">
+                            <button
+                                className="inline-block h-5 w-5 flex-none 
             rounded-full border-2"
-            ></button>
-            <p className="grow text-gray-600">
-              Complete online Javascript curse in bluuweb
-            </p>
-            <button>
-              <CrossIcon />
-            </button>
-          </article>
+                            ></button>
+                            <p className="grow text-gray-600">
+                                Complete online Javascript curse in bluuweb
+                            </p>
+                            <button>
+                                <CrossIcon />
+                            </button>
+                        </article>
+                    </div>
 
-          <section className="flex justify-between px-4 py-4">
-            <span className="text-gray-400 ">5 items left</span>
-            <button className="text-gray-400"> Clear Completed</button>
-          </section>
-        </div>
-      </main>
+                    {/* TodoComputed */}
 
-      <section className="container mx-auto mt-8 px-4">
-        <div className="flex justify-center gap-2 rounded-md bg-white p-4">
-          <button className="text-blue-600">All</button>
-          <button className="hover:text-blue-600">Active</button>
-          <button className="hover:text-blue-600">Completed</button>
-        </div>
-      </section>
+                    <section className="flex justify-between px-4 py-4">
+                        <span className="text-gray-400 ">5 items left</span>
+                        <button className="text-gray-400">
+                            {" "}
+                            Clear Completed
+                        </button>
+                    </section>
 
-      <p className="text-center">Drag and drop to reorder list</p>
-    </div>
-  );
+                    {/* TodoFilters */}
+
+                    <section className="container mx-auto mt-8 px-4">
+                        <div className="flex justify-center gap-2 rounded-md bg-white p-4">
+                            <button className="text-blue-600">All</button>
+                            <button className="hover:text-blue-600">
+                                Active
+                            </button>
+                            <button className="hover:text-blue-600">
+                                Completed
+                            </button>
+                        </div>
+                    </section>
+                </main>
+
+                <p className="text-center">Drag and drop to reorder list</p>
+            </div>
+        </>
+    );
 }
 
 export default App;
