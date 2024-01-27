0675fb1f7003fbce8f557f39c44f550f6886fd95
ramiro999 <ramiro2191920@correo.uis.edu.co>
2024-01-27 14:43:22 -0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import MoonIcon from "./components/MoonIcon";
 import CrossIcon from "./components/icons/CrossIcon";
 
 function App() {
@@ -13,11 +14,13 @@ function App() {
           <h1 className="text-3xl font-semibold uppercase tracking-[0.3rem] text-white">
             Todo
           </h1>
-          <button>Luna</button>
+          <button>
+            <MoonIcon className="fill-red-500" />
+          </button>
         </div>
 
         <form
-          className="mt-8 flex items-center gap-4 overflow-hidden
+          className=" mt-8 flex items-center gap-4 overflow-hidden
          rounded-md bg-white px-4 py-4"
         >
           <span className="inline-block h-5 w-5 rounded-full border-2"></span>
@@ -29,8 +32,8 @@ function App() {
         </form>
       </header>
 
-      <main className="container mx-auto px-4">
-        <div className="rounded-md bg-white">
+      <main className="container mx-auto mt-8 px-4">
+        <div className="rounded-md bg-white [&>article]:p-4 ">
           <article className="flex gap-4 border-b border-b-gray-400 px-4 py-4">
             <button
               className="inline-block h-5 w-5 flex-none 
@@ -70,17 +73,19 @@ function App() {
             </button>
           </article>
 
-          <section>
-            <span>5 items left</span>
-            <button> Clear Completed</button>
+          <section className="flex justify-between px-4 py-4">
+            <span className="text-gray-400 ">5 items left</span>
+            <button className="text-gray-400"> Clear Completed</button>
           </section>
         </div>
       </main>
 
-      <section className="px-auto container mx-auto">
-        <button>All</button>
-        <button>Active</button>
-        <button>Completed</button>
+      <section className="container mx-auto mt-8 px-4">
+        <div className="flex justify-center gap-2 rounded-md bg-white p-4">
+          <button className="text-blue-600">All</button>
+          <button className="hover:text-blue-600">Active</button>
+          <button className="hover:text-blue-600">Completed</button>
+        </div>
       </section>
 
       <p className="text-center">Drag and drop to reorder list</p>
