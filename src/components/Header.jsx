8fe68cd53d3f6dc8158e626a981f4ddea751a9b9
ramiro999 <ramiro2191920@correo.uis.edu.co>
2024-01-30 12:43:33 -0500

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,27 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 
 import IconMoon from "./icons/IconMoon";
 import IconSun from "./icons/IconSun";
 
-const Header = () => {
-    const refHeader = useRef(null); // el uso de useRef es para poder acceder a un elemento del DOM
+const initialStateDarkMode = localStorage.getItem("theme") === "dark";
 
-    const [darkMode, setDarkMode] = useState(true);
+const Header = () => {
+    //const refHeader = useRef(null); // el uso de useRef es para poder acceder a un elemento del DOM
+
+    const [darkMode, setDarkMode] = useState(initialStateDarkMode);
 
     useEffect(() => {
         if (darkMode) {
             document.documentElement.classList.add("dark");
-            refHeader.current.classList.add("bg-dark-blue");
+            localStorage.setItem("theme", "dark");
         } else {
             document.documentElement.classList.remove("dark");
-            refHeader.current.classList.remove("bg-dark-blue");
+            localStorage.setItem("theme", "light");
         }
     }, [darkMode]);
 
     return (
-        <header className="container mx-auto px-4" ref={refHeader}>
+        <header className="container mx-auto px-4">
             <div className="flex justify-between">
                 <h1 className="text-3xl font-semibold uppercase tracking-[0.3rem] text-white">
                     Todo
